710d38d32a451ee3d26af31e1a2ed6c372460265
dfasfds <143038172+habbba13@users.noreply.github.com>
2025-04-03 13:09:44 -0600

    Update lyrics.js

--- api/lyrics.js
+++ api/lyrics.js
@@ -2,7 +2,7 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-  res.setHeader(" "Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   const { title } = req.query;
   if (!title) {
